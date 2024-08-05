3c01a3cace333338975d6015b9bb7c3a7e7c167a
colspan <collin@collindigm.com>
2024-08-05 05:39:29 +0300

    Firebase Configuration updated

--- firebase.js
+++ firebase.js
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA4CCkjr19VIIcjyr53Fi3o5Gx3abHCr0k",
-  authDomain: "headstarterpantryproject.firebaseapp.com",
-  projectId: "headstarterpantryproject",
-  storageBucket: "headstarterpantryproject.appspot.com",
-  messagingSenderId: "127751747207",
-  appId: "1:127751747207:web:24f2d32bb9e55e829b6a22",
-  measurementId: "G-1QNW50LX7Q"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 
